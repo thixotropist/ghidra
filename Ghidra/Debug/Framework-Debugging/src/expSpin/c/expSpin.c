bd05cff4a220959bb42e5049d5c3619707d9b2a1
Gary Hayward <garyhayward@gmail.com>
2024-05-02 19:52:18 -0400

    Allow compilation under gcc 14.0.1 and Fedora 40

--- Ghidra/Debug/Framework-Debugging/src/expSpin/c/expSpin.c
+++ Ghidra/Debug/Framework-Debugging/src/expSpin/c/expSpin.c
@@ -24,6 +24,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 	}
 }
 #else
+#include <unistd.h>
 int main(int argc, char** argv) {
 	for (int i = 0; i < 10; i++) {
 		sleep(1);
